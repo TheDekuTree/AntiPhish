27f4cf5c273aa61dce42cbb918147804474b9c05
Nathan <nlaha@outlook.com>
2018-08-10 13:39:44 -0700

    Updated index.js with fancy color text

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
-const chalk = require('chalk');
-const readline = require('readline');
+const chalk = require("chalk");
+const readline = require("readline");
 
-var address = 'http://facebook.2rm299.trade/login.php';
+var address = "";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-rl.question('Please enter the address where the POST requests should be sent! (This is not the URL of the login page): ', (answer) => {
-    address = answer
+rl.question(
+  chalk.cyan(
+    "\nPlease enter the address where the POST requests should be sent! (This is not the URL of the login page): "
+  ),
+  answer => {
+    address = answer;
     rl.close();
-    start()
-});
+    start();
+  }
+);
 
 function start() {
-    console.log(chalk.green("\nStarting the spamming!"))
-    require('./spam.js').startSpamming(address)
+  console.log(chalk.green("\nStarting the spamming!"));
+  require("./spam.js").startSpamming(address);
 }
